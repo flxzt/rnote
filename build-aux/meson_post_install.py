6398906cfaf6a989b370ff7496a89270158c99a0
I Woithe <iwoithe@just42.net>
2022-08-01 20:06:41 +0930

    Automate installing build on Windows

--- build-aux/meson_post_install.py
+++ build-aux/meson_post_install.py
@@ -6,11 +6,24 @@ from subprocess import call
 if not environ.get('DESTDIR', ''):
     PREFIX = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
     DATA_DIR = path.join(PREFIX, 'share')
-    print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
-    print("Compiling new schemas...")
-    call(["glib-compile-schemas", path.join(DATA_DIR, 'glib-2.0/schemas')])
-    print("Updating desktop database...")
-    call(["update-desktop-database", path.join(DATA_DIR, 'applications')])
-    print("Updating MIME-type database...")
-    call(["update-mime-database", path.join(DATA_DIR, 'mime')])
+
+    if sys.platform.startswith('linux'):
+        print('Updating icon cache...')
+        call(['gtk-update-icon-cache', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
+        print("Compiling new schemas...")
+        call(["glib-compile-schemas", path.join(DATA_DIR, 'glib-2.0/schemas')])
+        print("Updating desktop database...")
+        call(["update-desktop-database", path.join(DATA_DIR, 'applications')])
+        print("Updating MIME-type database...")
+        call(["update-mime-database", path.join(DATA_DIR, 'mime')])
+    elif sys.platform == "win32" or sys.platform == "cygwin":
+        print('Updating icon cache...')
+        call(['gtk-update-icon-cache-3.0.exe', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
+        print("Compiling new schemas...")
+        call(["glib-compile-schemas.exe", path.join(DATA_DIR, 'glib-2.0/schemas')])
+        print("Updating desktop database...")
+        call(["update-desktop-database.exe", path.join(DATA_DIR, 'applications')])
+        print("Updating MIME-type database...")
+        call(["update-mime-database.exe", path.join(DATA_DIR, 'mime')])
+    else:
+        print(f"[WARNING] \"meson_post_install.py\" does not work on {sys.platform}")

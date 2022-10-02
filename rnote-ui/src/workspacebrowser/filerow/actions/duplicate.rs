6ddc6d5ed0fbc1f970a6d30fb2d6880c588cc7a7
TornaxO7 <tornax07@gmail.com>
2022-10-02 15:51:50 +0200

    add some little implementatio

--- rnote-ui/src/workspacebrowser/filerow/actions/duplicate.rs
+++ rnote-ui/src/workspacebrowser/filerow/actions/duplicate.rs
@@ -1,7 +1,12 @@
+use std::fs;
+use std::path::PathBuf;
+
 use gtk4::prelude::FileExt;
 use gtk4::{gio, glib, glib::clone};
 
 use crate::workspacebrowser::FileRow;
+
+const DUPLICATE_SUFFIX: &str = ".dup";
 
 impl FileRow {
     pub fn duplicate_action(&self) -> gio::SimpleAction {
@@ -11,12 +16,12 @@ impl FileRow {
             clone!(@weak self as filerow => move |_action_duplicate_file, _| {
                 if let Some(current_file) = filerow.current_file() {
                     if let Some(current_path) = current_file.path() {
-                        let path = current_path.into_boxed_path();
+                        let path = current_path.clone().into_boxed_path();
 
                         if path.is_dir() {
-                            duplicate_dir();
+                            duplicate_dir(current_path);
                         } else if path.is_file() {
-                            duplicate_file();
+                            duplicate_file(current_path);
                         } 
                     }
                 }
@@ -27,6 +32,19 @@ impl FileRow {
     }
 }
 
-fn duplicate_file() {}
+fn duplicate_file(source: PathBuf) {
+    let mut duplicate_name = {
+        let path = source.clone();
+        pat.push(DUPLICATE_SUFFIX);
+        path
+    };
+}
 
-fn duplicate_dir() {}
+fn duplicate_dir(_source: PathBuf) {}
+
+fn get_duplicate_name(name: PathBuf) -> PathBuf {
+    let path = name.into_boxed_path();
+
+    name.push(DUPLICATE_SUFFIX);
+    name
+}

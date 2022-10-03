742062ac4b7691f39a994f103a534df28334410e
TornaxO7 <tornax07@gmail.com>
2022-10-03 20:22:03 +0200

    finishing add-directory

--- rnote-ui/src/workspacebrowser/filerow/actions/add_dir.rs
+++ rnote-ui/src/workspacebrowser/filerow/actions/add_dir.rs
@@ -1,10 +1,13 @@
-use crate::workspacebrowser::{FileRow, filerow::widget_helper};
+use std::path::PathBuf;
+
+use crate::workspacebrowser::{filerow::widget_helper, FileRow};
 use gettextrs::gettext;
 use gtk4::{
     gio, glib,
     glib::clone,
     pango,
-    traits::{ButtonExt, EditableExt, PopoverExt, StyleContextExt, WidgetExt, BoxExt},
+    prelude::FileExt,
+    traits::{BoxExt, ButtonExt, EditableExt, PopoverExt, StyleContextExt, WidgetExt},
     Align, Button, Entry, Label, Popover,
 };
 
@@ -12,20 +15,23 @@ impl FileRow {
     pub fn add_dir_action(&self) -> gio::SimpleAction {
         let new_file = gio::SimpleAction::new("add-dir", None);
 
-        new_file.connect_activate(
-            clone!(@weak self as filerow => move |_action_rename_file, _| {
-                let entry = get_entry();
-                let label = get_label();
-                let (apply_button, popover) = widget_helper::entry_dialog::get_entry_dialog(&entry, &label);
+        new_file.connect_activate(clone!(@weak self as filerow => move |_action_rename_file, _| {
+            if let Some(current_file) = filerow.current_file() {
+                if let Some(current_path) = current_file.path() {
+                    if let Some(parent_path) = current_path.parent().map(|parent_path| parent_path.to_path_buf()) {
+                        let entry = get_entry();
+                        let label = get_label();
+                        let (apply_button, popover) = widget_helper::entry_dialog::get_entry_dialog(&entry, &label);
 
-                filerow.menubutton_box().append(&popover);
+                        filerow.menubutton_box().append(&popover);
 
-                connect_entry(&entry, &apply_button);
-                connect_apply_button(&apply_button, &popover, &entry);
+                        connect_apply_button(&apply_button, &popover, &entry, parent_path.clone());
 
-                popover.popup();
-            }),
-        );
+                        popover.popup();
+                    }
+                }
+            }
+        }));
 
         new_file
     }
@@ -39,7 +45,7 @@ fn get_label() -> Label {
     let label = Label::builder()
         .margin_bottom(12)
         .halign(Align::Center)
-        .label(&gettext("Rename"))
+        .label(&gettext("New Directory name"))
         .width_chars(24)
         .ellipsize(pango::EllipsizeMode::End)
         .build();
@@ -48,14 +54,25 @@ fn get_label() -> Label {
     label
 }
 
-fn connect_entry(entry: &Entry, apply_button: &Button) {
-    entry.connect_text_notify(clone!(@weak apply_button => move |entry| {
-        println!("Connect entry");
-    }));
-}
-
-fn connect_apply_button(apply_button: &Button, popover: &Popover, entry: &Entry) {
+fn connect_apply_button(
+    apply_button: &Button,
+    popover: &Popover,
+    entry: &Entry,
+    parent_path: PathBuf,
+) {
     apply_button.connect_clicked(clone!(@weak popover, @weak entry => move |_| {
-        println!("Connect apply button");
+        let new_file_path = parent_path.join(entry.text().to_string());
+        let new_file = gio::File::for_path(new_file_path.clone());
+
+        if new_file.query_exists(None::<&gio::Cancellable>) {
+            // Should have been caught earlier, but making sure
+            log::error!("Directory already exists.");
+        } else {
+            if let Err(e) = fs_extra::dir::create(new_file_path, false) {
+                log::error!("Couldn't create directory: {}", e);
+            }
+
+            popover.popdown();
+        }
     }));
 }

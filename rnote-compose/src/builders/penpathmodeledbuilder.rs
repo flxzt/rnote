d3687c00e0779fa780221443a1723407451876dd
Felix Zwettler <f.zwettler@posteo.de>
2022-10-16 16:41:59 +0200

    make stroke modeler input more robust, simplify builder

--- rnote-compose/src/builders/penpathmodeledbuilder.rs
+++ rnote-compose/src/builders/penpathmodeledbuilder.rs
@@ -3,7 +3,6 @@ use ink_stroke_modeler_rs::{
 };
 use p2d::bounding_volume::{BoundingVolume, AABB};
 use piet::RenderContext;
-use serde::{Deserialize, Serialize};
 use std::collections::VecDeque;
 use std::time::Instant;
 
@@ -15,29 +14,23 @@ use crate::{PenPath, Shape, Style};
 use super::shapebuilderbehaviour::{BuilderProgress, ShapeBuilderCreator};
 use super::{Constraints, ShapeBuilderBehaviour};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub(crate) enum PenPathModeledBuilderState {
-    Start,
-    During,
-}
-
 /// The pen path builder
 pub struct PenPathModeledBuilder {
-    pub(crate) state: PenPathModeledBuilderState,
     /// Buffered elements, which are filled up by new pen events and used to try to build path segments
     pub buffer: VecDeque<Element>,
     start_time: Instant,
-    last_fed_element: Element,
+    last_element: Element,
+    last_element_time: Instant,
     stroke_modeler: StrokeModeler,
 }
 
 impl std::fmt::Debug for PenPathModeledBuilder {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("ModeledPenPathBuilder")
-            .field("state", &self.state)
             .field("buffer", &self.buffer)
             .field("start_time", &self.start_time)
-            .field("last_fed_element", &self.last_fed_element)
+            .field("last_element", &self.last_element)
+            .field("last_element_time", &self.last_element_time)
             .field("stroke_modeler", &".. no debug impl ..")
             .finish()
     }
@@ -45,42 +38,22 @@ impl std::fmt::Debug for PenPathModeledBuilder {
 
 impl ShapeBuilderCreator for PenPathModeledBuilder {
     fn start(element: Element, now: Instant) -> Self {
-        let mut buffer = VecDeque::new();
+        let now = Instant::now();
+        let buffer = VecDeque::new();
 
-        let modeler_params = StrokeModelerParams {
-            // Increase the time that can pass between inputs (happens when resting on a position while drawing)
-            sampling_max_outputs_per_call: 100,
-            ..Default::default()
-        };
+        let stroke_modeler = StrokeModeler::default();
 
-        let mut stroke_modeler = StrokeModeler::new(modeler_params);
-
-        buffer.extend(
-            stroke_modeler
-                .update(ModelerInput::new(
-                    ModelerInputEventType::kDown,
-                    (element.pos[0] as f32, element.pos[1] as f32),
-                    0.0,
-                    element.pressure as f32,
-                    0.0,
-                    0.0,
-                ))
-                .into_iter()
-                .map(|r| {
-                    let pos = r.get_pos();
-                    let pressure = r.get_pressure();
-
-                    Element::new(na::vector![pos.0 as f64, pos.1 as f64], pressure as f64)
-                }),
-        );
-
-        Self {
-            state: PenPathModeledBuilderState::Start,
+        let mut builder = Self {
             buffer,
             start_time: now,
-            last_fed_element: element,
+            last_element: element,
+            last_element_time: now,
             stroke_modeler,
-        }
+        };
+
+        builder.restart(element, now);
+
+        builder
     }
 }
 
@@ -98,8 +71,8 @@ impl ShapeBuilderBehaviour for PenPathModeledBuilder {
             self.state
         ); */
 
-        match (&mut self.state, event) {
-            (PenPathModeledBuilderState::Start, PenEvent::Down { element, .. }) => {
+        match event {
+            PenEvent::Down { element, .. } => {
                 // kDown is already fed when instanciating the builder
                 self.update_modeler_w_element(element, ModelerInputEventType::kMove);
 
@@ -108,31 +81,17 @@ impl ShapeBuilderBehaviour for PenPathModeledBuilder {
                     None => BuilderProgress::InProgress,
                 }
             }
-            (PenPathModeledBuilderState::During, PenEvent::Down { element, .. }) => {
-                self.update_modeler_w_element(element, ModelerInputEventType::kMove);
-
-                match self.try_build_segments() {
-                    Some(shapes) => BuilderProgress::EmitContinue(shapes),
-                    None => BuilderProgress::InProgress,
-                }
-            }
-            (_, PenEvent::Up { element, .. }) => {
+            PenEvent::Up { element, .. } => {
                 self.update_modeler_w_element(element, ModelerInputEventType::kUp);
 
                 let segment = self.try_build_segments().unwrap_or_else(|| vec![]);
 
-                self.reset();
-
                 BuilderProgress::Finished(segment)
             }
-            (_, PenEvent::Proximity { .. })
-            | (_, PenEvent::KeyPressed { .. })
-            | (_, PenEvent::Text { .. }) => BuilderProgress::InProgress,
-            (_, PenEvent::Cancel) => {
-                self.reset();
-
-                BuilderProgress::Finished(vec![])
+            PenEvent::Proximity { .. } | PenEvent::KeyPressed { .. } | PenEvent::Text { .. } => {
+                BuilderProgress::InProgress
             }
+            PenEvent::Cancel => BuilderProgress::Finished(vec![]),
         }
     }
 
@@ -151,39 +110,27 @@ impl ShapeBuilderBehaviour for PenPathModeledBuilder {
 
     fn draw_styled(&self, cx: &mut piet_cairo::CairoRenderContext, style: &Style, _zoom: f64) {
         cx.save().unwrap();
-        let penpath = match &self.state {
-            PenPathModeledBuilderState::Start => self
-                .buffer
-                .iter()
-                .zip(self.buffer.iter().skip(1))
-                .map(|(start, end)| Segment::Line {
-                    start: *start,
-                    end: *end,
-                })
-                .collect::<PenPath>(),
-            PenPathModeledBuilderState::During => {
-                let prediction = self
-                    .stroke_modeler
-                    .predict()
-                    .into_iter()
-                    .map(|r| {
-                        let pos = r.get_pos();
-                        let pressure = r.get_pressure();
 
-                        Element::new(na::vector![pos.0 as f64, pos.1 as f64], pressure as f64)
-                    })
-                    .collect::<Vec<Element>>();
+        let prediction = self
+            .stroke_modeler
+            .predict()
+            .into_iter()
+            .map(|r| {
+                let pos = r.get_pos();
+                let pressure = r.get_pressure();
 
-                prediction
-                    .iter()
-                    .zip(prediction.iter().skip(1))
-                    .map(|(start, end)| Segment::Line {
-                        start: *start,
-                        end: *end,
-                    })
-                    .collect::<PenPath>()
-            }
-        };
+                Element::new(na::vector![pos.0 as f64, pos.1 as f64], pressure as f64)
+            })
+            .collect::<Vec<Element>>();
+
+        let penpath = prediction
+            .iter()
+            .zip(prediction.iter().skip(1))
+            .map(|(start, end)| Segment::Line {
+                start: *start,
+                end: *end,
+            })
+            .collect::<PenPath>();
 
         /*
                // Change prediction stroke color for debugging
@@ -202,6 +149,9 @@ impl ShapeBuilderBehaviour for PenPathModeledBuilder {
 }
 
 impl PenPathModeledBuilder {
+    const MODELER_MIN_OUTPUT_RATE: f64 = 180.0;
+    const MODELER_MAX_OUTPUTS_PER_CALL: i32 = 100;
+
     fn try_build_segments(&mut self) -> Option<Vec<Shape>> {
         if self.buffer.len() < 2 {
             return None;
@@ -209,8 +159,6 @@ impl PenPathModeledBuilder {
         let mut segments = vec![];
 
         while self.buffer.len() > 2 {
-            self.state = PenPathModeledBuilderState::During;
-
             segments.push(Shape::Segment(Segment::Line {
                 start: self.buffer[0],
                 end: self.buffer[1],
@@ -223,17 +171,31 @@ impl PenPathModeledBuilder {
     }
 
     fn update_modeler_w_element(&mut self, element: Element, event_type: ModelerInputEventType) {
-        if self.last_fed_element == element {
-            // Can't feed modeler with duplicate elements, will result in `INVALID_ARGUMENT` errors
+        let now = Instant::now();
+
+        if self.last_element == element {
+            // Can't feed modeler with duplicate elements, or results in `INVALID_ARGUMENT` errors
             return;
-        } else {
-            self.last_fed_element = element;
         }
+        self.last_element = element;
+
+        let n_steps = (now.duration_since(self.last_element_time).as_secs_f64()
+            * Self::MODELER_MIN_OUTPUT_RATE)
+            .ceil() as i32;
+
+        if n_steps > Self::MODELER_MAX_OUTPUTS_PER_CALL {
+            // If the no of outputs the modeler would need to produce exceeds the configured maximum
+            // ( because the time delta between the last elements is too large ), it needs to be restarted.
+            log::info!("penpathmodeledbuilder: update_modeler_w_element(): n_steps exceeds configured max outputs per call, restarting modeler");
+
+            self.restart(element, now);
+        }
+        self.last_element_time = now;
 
         let modeler_input = ModelerInput::new(
             event_type,
             (element.pos[0] as f32, element.pos[1] as f32),
-            self.start_time.elapsed().as_secs_f64(),
+            now.duration_since(self.start_time).as_secs_f64(),
             element.pressure as f32,
             0.0,
             0.0,
@@ -254,8 +216,36 @@ impl PenPathModeledBuilder {
         );
     }
 
-    fn reset(&mut self) {
+    fn restart(&mut self, element: Element, now: Instant) {
+        let params = StrokeModelerParams {
+            sampling_min_output_rate: Self::MODELER_MIN_OUTPUT_RATE,
+            sampling_max_outputs_per_call: Self::MODELER_MAX_OUTPUTS_PER_CALL,
+            ..Default::default()
+        };
+
         self.buffer.clear();
-        self.state = PenPathModeledBuilderState::Start;
+        self.start_time = now;
+        self.last_element_time = now;
+        self.last_element = element;
+        self.stroke_modeler.reset(params);
+
+        self.buffer.extend(
+            self.stroke_modeler
+                .update(ModelerInput::new(
+                    ModelerInputEventType::kDown,
+                    (element.pos[0] as f32, element.pos[1] as f32),
+                    0.0,
+                    element.pressure as f32,
+                    0.0,
+                    0.0,
+                ))
+                .into_iter()
+                .map(|r| {
+                    let pos = r.get_pos();
+                    let pressure = r.get_pressure();
+
+                    Element::new(na::vector![pos.0 as f64, pos.1 as f64], pressure as f64)
+                }),
+        );
     }
 }

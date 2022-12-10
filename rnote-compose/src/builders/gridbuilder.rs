1874dccc9a99d2bcc4b715ef356272939bad287d
Felix Zwettler <f.zwettler@posteo.de>
2022-12-10 11:51:27 +0100

    draw indicators

--- rnote-compose/src/builders/gridbuilder.rs
+++ rnote-compose/src/builders/gridbuilder.rs
@@ -4,10 +4,10 @@ use p2d::bounding_volume::{BoundingVolume, AABB};
 use piet::RenderContext;
 
 use crate::helpers::AABBHelpers;
-use crate::penhelpers::PenEvent;
+use crate::penhelpers::{PenEvent, PenState};
 use crate::penpath::Element;
 use crate::shapes::{Line, Rectangle};
-use crate::style::Composer;
+use crate::style::{drawhelpers, Composer};
 use crate::{Shape, Style};
 
 use super::shapebuilderbehaviour::{ShapeBuilderCreator, ShapeBuilderProgress};
@@ -84,8 +84,8 @@ impl ShapeBuilderBehaviour for GridBuilder {
         ShapeBuilderProgress::InProgress
     }
 
-    fn bounds(&self, style: &Style, _zoom: f64) -> Option<AABB> {
-        let bounds_margin = style.bounds_margin();
+    fn bounds(&self, style: &Style, zoom: f64) -> Option<AABB> {
+        let bounds_margin = style.bounds_margin().max(drawhelpers::POS_INDICATOR_RADIUS) / zoom;
 
         match &self.state {
             GridBuilderState::Start(start) => Some(AABB::from_half_extents(
@@ -100,7 +100,7 @@ impl ShapeBuilderBehaviour for GridBuilder {
         }
     }
 
-    fn draw_styled(&self, cx: &mut piet_cairo::CairoRenderContext, style: &Style, _zoom: f64) {
+    fn draw_styled(&self, cx: &mut piet_cairo::CairoRenderContext, style: &Style, zoom: f64) {
         cx.save().unwrap();
 
         let mut style = style.clone();
@@ -109,6 +109,36 @@ impl ShapeBuilderBehaviour for GridBuilder {
             line.draw_composed(cx, &style);
 
             style.advance_seed();
+        }
+
+        match &self.state {
+            GridBuilderState::Start(start) => {
+                drawhelpers::draw_pos_indicator(cx, PenState::Down, *start, zoom);
+            }
+            GridBuilderState::FirstCell { start, current } => {
+                drawhelpers::draw_pos_indicator(cx, PenState::Up, *start, zoom);
+                drawhelpers::draw_pos_indicator(cx, PenState::Down, *current, zoom);
+            }
+            GridBuilderState::Grids {
+                start,
+                cell_size,
+                current,
+            } => {
+                drawhelpers::draw_pos_indicator(cx, PenState::Up, *start, zoom);
+
+                let cols = ((current - start)[0] / cell_size[0]).floor();
+                let rows = ((current - start)[1] / cell_size[1]).floor();
+
+                if cols > 0.0 && rows > 0.0 {
+                    drawhelpers::draw_pos_indicator(cx, PenState::Up, *start + cell_size, zoom);
+                    drawhelpers::draw_pos_indicator(
+                        cx,
+                        PenState::Up,
+                        *start + cell_size.component_mul(&na::vector![cols, rows]),
+                        zoom,
+                    );
+                }
+            }
         }
 
         cx.restore().unwrap();
@@ -139,9 +169,10 @@ impl GridBuilder {
                         return vec![];
                     }
 
-                    (cols.abs().floor() as u32, rows.abs().floor() as u32)
+                    (cols.floor() as u32, rows.floor() as u32)
                 };
 
+                // lines of the upper side
                 let mut lines = (0..cols)
                     .map(|col| Line {
                         start: na::vector![start[0] + cell_size[0] * col as f64, start[1]],
@@ -149,11 +180,13 @@ impl GridBuilder {
                     })
                     .collect::<Vec<Line>>();
 
+                // lines of the left side
                 lines.extend((0..rows).map(|row| Line {
                     start: na::vector![start[0], start[1] + cell_size[1] * row as f64],
                     end: na::vector![start[0], start[1] + cell_size[1] * (row + 1) as f64],
                 }));
 
+                // cell outlines
                 lines.extend((0..rows).flat_map(move |row| {
                     (0..cols).flat_map(move |col| {
                         let corner =

f39315ecfd59f2c9f4bfd7eaa5d9b2488d55ec7c
Felix Zwettler <f.zwettler@posteo.de>
2023-01-24 14:55:33 +0100

    add setup_macos_env() for macos app bundles

--- rnote-ui/src/main.rs
+++ rnote-ui/src/main.rs
@@ -42,14 +42,19 @@ pub(crate) use strokewidthpicker::RnStrokeWidthPicker;
 pub(crate) use unitentry::RnUnitEntry;
 pub(crate) use workspacebrowser::RnWorkspaceBrowser;
 
-use gtk4::prelude::*;
 extern crate nalgebra as na;
 extern crate parry2d_f64 as p2d;
+
+use gtk4::prelude::*;
 
 fn main() -> anyhow::Result<()> {
     #[cfg(target_os = "windows")]
     if let Err(e) = setup_windows_env() {
         eprintln!("failed to setup env for windows, Err: {e:?}");
+    }
+    #[cfg(target_os = "macos")]
+    if let Err(e) = setup_macos_env() {
+        eprintln!("failed to setup env for macos, Err: {e:?}");
     }
 
     let app = RnApp::new();
@@ -75,5 +80,34 @@ fn setup_windows_env() -> anyhow::Result<()> {
     );
     // for debugging
     //std::env::set_var("RUST_LOG", "rnote=debug");
+    Ok(())
+}
+
+/// we need to set some env vars for macos app bundles
+#[cfg(target_os = "macos")]
+fn setup_macos_env() -> anyhow::Result<()> {
+    use std::ffi::OsStr;
+    use std::path::{Component, PathBuf};
+
+    let current_dir = std::env::current_dir()?.canonicalize()?;
+    if current_dir
+        .components()
+        .zip(current_dir.components().skip(1))
+        .any(|(a, b)| {
+            if let (Component::Normal(a), Component::Normal(b)) = (a, b) {
+                a == OsStr::new("Contents") && b == OsStr::new("MacOS")
+            } else {
+                false
+            }
+        })
+    {
+        std::env::set_var("XDG_DATA_DIRS", &current_dir.join("/..Resources/share"));
+        std::env::set_var(
+            "GDK_PIXBUF_MODULE_FILE",
+            current_dir.join(PathBuf::from(
+                "/../Resources/lib/gdk-pixbuf-2.0/2.10.0/loaders/loaders.cache",
+            )),
+        );
+    }
     Ok(())
 }

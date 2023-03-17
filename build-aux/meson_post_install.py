8585b446c08b246f3d55359026415cb3d242d44e
Robert Schütz <github@dotlambda.de>
2023-03-16 23:31:40 -0700

    handle macOS in meson_post_install.py

--- build-aux/meson_post_install.py
+++ build-aux/meson_post_install.py
@@ -11,7 +11,7 @@ bindir = sys.argv[2]
 app_bin = sys.argv[3]
 
 if not os.environ.get('DESTDIR', ''):
-    if sys.platform.startswith('linux'):
+    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
         print('Updating icon cache...', file=sys.stderr)
         call(['gtk-update-icon-cache', '-qtf', os.path.join(datadir, 'icons/hicolor')])
         print("Compiling new schemas...", file=sys.stderr)

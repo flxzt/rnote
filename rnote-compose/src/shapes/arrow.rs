8186063d6c7328b9100089ebf2293cc8c72fcce8
TornaxO7 <tornax@pm.me>
2023-04-08 23:19:29 +0200

    arrow: removing `get_rotation_matrix` function

--- rnote-compose/src/shapes/arrow.rs
+++ rnote-compose/src/shapes/arrow.rs
@@ -133,7 +133,7 @@ impl Arrow {
     pub fn compute_lline(&self, stroke_width: Option<f64>) -> na::Vector2<f64> {
         let vec_a =
             self.compute_stem_direction_vector() * Self::compute_tip_lines_length(stroke_width);
-        let rotation_matrix = self.get_rotation_matrix();
+        let rotation_matrix = Rotation2::new(Self::TIP_LINES_STEM_ACUTE_ANGLE);
 
         rotation_matrix * vec_a + self.tip
     }
@@ -143,7 +143,7 @@ impl Arrow {
     pub fn compute_rline(&self, stroke_width: Option<f64>) -> na::Vector2<f64> {
         let vec_b =
             self.compute_stem_direction_vector() * Self::compute_tip_lines_length(stroke_width);
-        let rotation_matrix = self.get_rotation_matrix().transpose();
+        let rotation_matrix = Rotation2::new(Self::TIP_LINES_STEM_ACUTE_ANGLE);
 
         rotation_matrix * vec_b + self.tip
     }
@@ -173,12 +173,6 @@ impl Arrow {
         } else {
             direction_vector / direction_vector.norm()
         }
-    }
-
-    /// Returns the rotation matrix for the tip lines how they should be
-    /// rotated.
-    fn get_rotation_matrix(&self) -> Rotation2<f64> {
-        Rotation2::new(Self::TIP_LINES_STEM_ACUTE_ANGLE)
     }
 
     /// Computes and returns the length of the tip lines with the given stroke

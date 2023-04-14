9eb94a8a270b32504c51bd65515faf9eef0413f8
Kneemund <niemand@ymir-graphics.com>
2023-04-15 01:42:53 +0200

    change pixbuf directories

--- build-aux/inno_build.py
+++ build-aux/inno_build.py
@@ -26,8 +26,8 @@ if not os.path.exists(dll_directory):
     os.mkdir(dll_directory)
 
 print("Collecting DLLs...", file=sys.stderr)
-os.system(f"ldd {os.path.join(build_root, 'rnote.exe')} | grep '\/mingw.*\.dll' -o | xargs -i cp {{}} {dll_directory}")
-os.system(f"ldd {os.path.join(msys_path, '/mingw64/lib/gdk-pixbuf-2.0/2.10.0/loaders/*.dll')} | grep '\/mingw.*\.dll' -o | xargs -i cp {{}} {dll_directory}")
+os.system(f"ldd {os.path.join(build_root, 'rnote.exe')} | grep '\\/mingw.*\.dll' -o | xargs -i cp {{}} {dll_directory}")
+os.system(f"ldd {msys_path}/mingw64/lib/gdk-pixbuf-2.0/2.10.0/loaders/*.dll | grep '\\/mingw.*\.dll' -o | xargs -i cp {{}} {dll_directory}")
 
 print("Running ISCC...", file=sys.stderr)
 # TODO: maybe use chocolatey to install in workflow? it's not added to PATH by default, hardcoding is a bad idea.

4316551f53ba2bc4052478e85fd1fbdc9723ddab
Kneemund <niemand@ymir-graphics.com>
2023-04-16 14:14:18 +0200

    use app_output from meson, rename installer

--- build-aux/inno_build.py
+++ build-aux/inno_build.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 inno_script = sys.argv[1]
-installer_output = sys.argv[2]
+app_output = sys.argv[2]
 source_root = sys.argv[3]
 build_root = sys.argv[4]
 msys_path = sys.argv[5]
@@ -12,7 +12,7 @@ msys_path = sys.argv[5]
 print(f"""
 ### executing Inno-Setup installer build script with arguments: ###
     inno_script: {inno_script}
-    installer_output: {installer_output}
+    app_output: {app_output}
     source_root: {source_root}
     build_root: {build_root}
     msys_path: {msys_path}
@@ -26,7 +26,7 @@ if not os.path.exists(dll_directory):
 
 # Don't use os.path.join here, because that uses the wrong separators which breaks wildcard expansion.
 print("Collecting DLLs...", file=sys.stderr)
-os.system(f"ldd {build_root}/rnote.exe | grep '\\/mingw.*\.dll' -o | xargs -i cp {{}} {dll_directory}")
+os.system(f"ldd {build_root}/{app_output} | grep '\\/mingw.*\.dll' -o | xargs -i cp {{}} {dll_directory}")
 os.system(f"ldd {msys_path}/mingw64/lib/gdk-pixbuf-2.0/2.10.0/loaders/*.dll | grep '\\/mingw.*\.dll' -o | xargs -i cp {{}} {dll_directory}")
 
 print("Running ISCC...", file=sys.stderr)

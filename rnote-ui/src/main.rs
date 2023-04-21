70a92b549174d274a29b000a1708d30319ccf185
Felix Zwettler <f.zwettler@posteo.de>
2023-04-21 15:40:39 +0200

    move some of setup previous to app startup

--- rnote-ui/src/main.rs
+++ rnote-ui/src/main.rs
@@ -49,10 +49,36 @@ extern crate parry2d_f64 as p2d;
 use gtk4::{glib, prelude::*};
 
 fn main() -> glib::ExitCode {
+    if let Err(e) = setup_logging() {
+        eprintln!("failed to setup logging, Err: {e:?}");
+    }
+
+    std::env::set_var("RUST_LOG", "rnote=debug");
+
     if let Err(e) = env::setup_env() {
         eprintln!("failed to setup env, Err: {e:?}");
     }
 
+    #[cfg(not(target_os = "windows"))]
+    if let Err(e) = setup_i18n() {
+        eprintln!("failed to setup i18n, Err: {e:?}");
+    }
+
     let app = RnApp::new();
     app.run()
+}
+
+fn setup_logging() -> anyhow::Result<()> {
+    pretty_env_logger::try_init_timed()?;
+    log::debug!("... env_logger initialized");
+    Ok(())
+}
+
+fn setup_i18n() -> anyhow::Result<()> {
+    let locale_dir = env::locale_dir()?;
+
+    gettextrs::setlocale(gettextrs::LocaleCategory::LcAll, "");
+    gettextrs::bindtextdomain(config::GETTEXT_PACKAGE, locale_dir)?;
+    gettextrs::textdomain(config::GETTEXT_PACKAGE)?;
+    Ok(())
 }

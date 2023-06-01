b100a56adef0a09ef3dacfbb406764be7e260344
LeSnake <dev.lesnake@posteo.de>
2023-06-01 17:02:49 +0200

    fix merge again

--- rnote-ui/src/mainheader.rs
+++ rnote-ui/src/mainheader.rs
@@ -18,8 +18,6 @@ mod imp {
         #[template_child]
         pub(crate) main_title_unsaved_recovery_indicator: TemplateChild<Label>,
         #[template_child]
-        pub(crate) fixedsize_quickactions_box: TemplateChild<gtk4::Box>,
-        #[template_child]
         pub(crate) left_flapreveal_toggle: TemplateChild<ToggleButton>,
         #[template_child]
         pub(crate) right_flapreveal_toggle: TemplateChild<ToggleButton>,
@@ -87,10 +85,6 @@ impl RnMainHeader {
 
     pub(crate) fn main_title_unsaved_recovery_indicator(&self) -> Label {
         self.imp().main_title_unsaved_recovery_indicator.get()
-    }
-
-    pub(crate) fn fixedsize_quickactions_box(&self) -> gtk4::Box {
-        self.imp().fixedsize_quickactions_box.get()
     }
 
     pub(crate) fn left_flapreveal_toggle(&self) -> ToggleButton {

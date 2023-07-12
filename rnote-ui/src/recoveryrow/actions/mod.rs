d3b0d1a417da94fd562dd67355f0929f2d345a5c
LeSnake04 <dev.lesnake@posteo.de>
2023-07-12 23:06:50 +0200

    removed recoveryrow

--- rnote-ui/src/recoveryrow/actions/mod.rs
+++
@@ -1,7 +0,0 @@
-mod discard;
-mod recover;
-mod save_as;
-
-pub(crate) use discard::discard;
-pub(crate) use recover::recover;
-pub(crate) use save_as::save_as;
--- rnote-ui/src/recoveryrow/actions/recover.rs
+++
@@ -1,5 +0,0 @@
-use crate::{appwindow::RnAppWindow, recoveryrow::RnRecoveryRow};
-
-pub(crate) async fn recover(recoveryrow: &RnRecoveryRow, appwindow: &RnAppWindow) {
-    todo!()
-}

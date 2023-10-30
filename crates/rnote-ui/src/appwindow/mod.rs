a8a3224322de55eebbcd3be59a166cb0c70b092b
Silvan Schmidt <sil.schmidt@proton.me>
2023-10-30 18:47:11 +0100

    fix: Loading bar didn't stop when the file was openend

--- crates/rnote-ui/src/appwindow/mod.rs
+++ crates/rnote-ui/src/appwindow/mod.rs
@@ -451,7 +451,8 @@ impl RnAppWindow {
                         appwindow.overlays().tabview().set_selected_page(&page);
                         false
                     } else {
-                        let rnote_file_new_tab = if appwindow.active_tab_wrapper().canvas().empty() {
+                        let rnote_file_new_tab = if appwindow.active_tab_wrapper().canvas().empty()
+                        {
                             false
                         } else {
                             rnote_file_new_tab
@@ -462,7 +463,6 @@ impl RnAppWindow {
                             appwindow.new_canvas_wrapper()
                         } else {
                             appwindow.active_tab_wrapper()
-
                         };
                         let (bytes, _) = input_file.load_bytes_future().await?;
                         let widget_flags = wrapper

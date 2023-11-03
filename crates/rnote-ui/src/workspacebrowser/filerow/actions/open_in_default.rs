91327e25721825198b5f9960314ed7ca48f4dfa3
Silvan Schmidt <sil.schmidt@proton.me>
2023-11-03 14:20:35 +0100

    Implement functionality

--- crates/rnote-ui/src/workspacebrowser/filerow/actions/open_in_default.rs
+++ crates/rnote-ui/src/workspacebrowser/filerow/actions/open_in_default.rs
@@ -1,14 +1,14 @@
 // Imports
 use crate::workspacebrowser::RnFileRow;
-use gtk4::{gio, glib, glib::clone};
+use gtk4::{gio, gio::prelude::FileExt, glib, glib::clone};
 
 /// Create a new `open-in-default` action.
 pub(crate) fn open_in_default(filerow: &RnFileRow) -> gio::SimpleAction {
     let action_open_in_default = gio::SimpleAction::new("open-in-default", None);
     action_open_in_default.connect_activate(
         clone!(@weak filerow => move |_action_open_in_default, _| {
-            if let Some(_current_file) = filerow.current_file() {
-                log::debug!("Open in default");
+            if let Some(current_file) = filerow.current_file() {
+                let _ = open::that(current_file.uri());
             }
         }),
     );

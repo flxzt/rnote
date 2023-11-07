2a129365a931b73f51c60ef1d3f7b883a946c0b4
Doublonmousse <e0949142@u.nus.edu>
2023-11-07 20:29:34 +0800

    natord variant for compare

--- crates/rnote-ui/src/workspacebrowser/mod.rs
+++ crates/rnote-ui/src/workspacebrowser/mod.rs
@@ -17,6 +17,7 @@ use gtk4::{
     PropertyExpression, ScrolledWindow, Separator, SignalListItemFactory, SingleSelection,
     SortListModel, SorterChange, Widget,
 };
+use natord::compare;
 use std::cell::RefCell;
 use std::path::{Path, PathBuf};
 
@@ -396,7 +397,9 @@ impl RnWorkspaceBrowser {
             let second_display_name = second_file.basename().unwrap();
             let second_display_name = second_display_name.to_str().unwrap();
 
-            first_display_name.cmp(second_display_name).into()
+            // first_display_name.cmp(second_display_name).into()
+            // replaced by
+            natord::compare(first_display_name, second_display_name).into()
         });
 
         let multisorter = MultiSorter::new();

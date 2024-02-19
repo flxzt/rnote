228b9fef394b1acff1b30499ccb6edd8c977fcc3
Doublonmousse <115779707+Doublonmousse@users.noreply.github.com>
2024-02-19 12:35:55 +0100

    modularize the `resize` file

--- crates/rnote-engine/src/strokes/resize.rs
+++ crates/rnote-engine/src/strokes/resize.rs
@@ -1,5 +1,4 @@
 use na;
-use p2d::query::gjk::eps_tol;
 
 /// Enum that lists the different options for sizing the image
 ///
@@ -31,6 +30,15 @@ pub struct Resize {
     pub respect_borders: bool,
 }
 
+/// Helper function to calculate ratios
+fn helper_calculate_fit_ratio(
+    max_position: &f64,
+    current_position: &f64,
+    current_size: &f64,
+) -> f64 {
+    (max_position - current_position) / current_size
+}
+
 /// Calculate the `ratio` by which to resize the image such that
 /// - it stays fully in view
 /// - it does not goes over a page border when the mode has a fixed
@@ -50,42 +58,81 @@ pub fn calculate_resize_ratio(
     let current_width = initial_size_image.x;
     let current_height = initial_size_image.y;
 
-    let mut ratio = 1.0f64; //start the ratio at 1
+    // compile all ratio in a vec
+    let ratios = vec![
+        // check that we do not go out of the canvas view in the x direction
+        helper_calculate_fit_ratio(
+            &resize.max_viewpoint.x,
+            &pos_left_top_canvas.index(0),
+            &current_width,
+        ),
+        // check that we do not go out of view in the y direction
+        helper_calculate_fit_ratio(
+            &resize.max_viewpoint.y,
+            &pos_left_top_canvas.index(1),
+            &current_height,
+        ),
+        // check if we go out of the page on the right on fixed layout
+        helper_calculate_fit_ratio(&resize.width, &pos_left_top_canvas.index(0), &current_width),
+        // check if we have to respect borders
+        calculate_resize_ratio_respect_borders(&resize, &initial_size_image, &pos_left_top_canvas),
+    ];
 
-    // check that we do not go out of the canvas view in the x direction
-    ratio = ratio.min((resize.max_viewpoint.x - pos_left_top_canvas.index(0)) / (current_width));
-    // check that we do not go out of view in the y direction
-    ratio = ratio.min((resize.max_viewpoint.y - pos_left_top_canvas.index(1)) / (current_height));
+    // apply rules
+    let apply_ratios = vec![
+        true,                   //canvas in the x direction
+        true,                   //canvas in the y direction
+        resize.isfixed_layout,  //do not go over the page on the right for fixed layout
+        resize.respect_borders, //do not go over the page on the right for all layouts (slightly redundant)
+    ];
 
-    // check if we go out of the page on the right on fixed layout
-    if resize.isfixed_layout {
-        ratio = ratio.min((resize.width - pos_left_top_canvas.index(0)) / (current_width));
-    }
-
-    // check if we have to respect borders
-    if resize.respect_borders {
-        ratio = ratio.min(calculate_resize_ratio_respect_borders(
-            resize,
-            initial_size_image,
-            pos_left_top_canvas,
-        ));
-    }
-    ratio
+    ratios
+        .iter()
+        .zip(apply_ratios)
+        .filter(|x| x.1)
+        .fold(1.0f64, |acc, x| acc.min(*x.0))
+        .max(1e-8f64) //force the value to be positive as a zero would incurr crashes when applying the transforms
 }
 
 /// calculate the ratio to not go over borders
 pub fn calculate_resize_ratio_respect_borders(
-    resize: Resize,
-    initial_size_image: na::Vector2<f64>,
-    pos_left_top_canvas: na::Vector2<f64>,
+    resize: &Resize,
+    initial_size_image: &na::Vector2<f64>,
+    pos_left_top_canvas: &na::Vector2<f64>,
 ) -> f64 {
-    // beware : we MIGHT have zero as the top position, so we start at at minimum at eps
-    let next_page_vertical_border =
-        (pos_left_top_canvas.index(0).max(eps_tol()) / resize.width).ceil() * resize.width;
-    let next_page_horizontal_border =
-        (pos_left_top_canvas.index(1).max(eps_tol()) / resize.height).ceil() * resize.height;
+    // closure to calculate the ceil
+    // We take the `floor (ratio + eps) + 1``
+    // This allows for element that would fall exactly
+    // on a border to be on the next page and disallow
+    // too small ratios
+    let f_ratio = |position: &f64, size: &f64| -> f64 {
+        (((position / size) + 1e-8f64).floor() + 1.0f64) * size
+    };
 
-    ((next_page_vertical_border - pos_left_top_canvas.index(0)) / initial_size_image.x)
-        .min((next_page_horizontal_border - pos_left_top_canvas.index(1)) / initial_size_image.y)
-        .max(eps_tol())
+    let next_page_vertical_border = f_ratio(&pos_left_top_canvas.index(0), &resize.width);
+    let next_page_horizontal_border = f_ratio(&pos_left_top_canvas.index(1), &resize.height);
+
+    let ratios = vec![
+        helper_calculate_fit_ratio(
+            &next_page_vertical_border,
+            &pos_left_top_canvas.index(0),
+            &initial_size_image.x,
+        ),
+        helper_calculate_fit_ratio(
+            &next_page_horizontal_border,
+            &pos_left_top_canvas.index(1),
+            &initial_size_image.y,
+        ),
+    ];
+
+    let rule_apply = vec![
+        true, // vertical rule
+        true, // horizontal rule
+    ];
+
+    ratios
+        .iter()
+        .zip(rule_apply)
+        .fold(1.0f64, |acc, x| acc.min(*x.0))
+        .max(1e-8f64)
 }

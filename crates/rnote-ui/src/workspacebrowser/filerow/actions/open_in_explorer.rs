70344769d9fcc37bcf6932b19b66affe225753d7
Doublonmousse <115779707+Doublonmousse@users.noreply.github.com>
2024-05-04 23:46:03 +0200

    improved error handling

--- crates/rnote-ui/src/workspacebrowser/filerow/actions/open_in_explorer.rs
+++ crates/rnote-ui/src/workspacebrowser/filerow/actions/open_in_explorer.rs
@@ -10,9 +10,16 @@ pub(crate) fn open_in_explorer(filerow: &RnFileRow, appwindow: &RnAppWindow) -> 
     action_open_in_default.connect_activate(
         clone!(@weak filerow, @weak appwindow => move |_action_open_in_default, _| {
             if let Some(current_file) = filerow.current_file() {
-                if let Err(e) = opener::reveal(current_file.path().unwrap()) {
+                // check if the path can be obtained
+                if let Some(path) = current_file.path() {
+                    if let Err(e) = opener::reveal(path ) {
+                        appwindow.overlays().dispatch_toast_error(&gettext("Failed to open the file in the file explorer"));
+                        tracing::debug!("opening file {} in the file explorer failed: {e:?}", current_file.uri());
+                    }
+                }
+                else {
                     appwindow.overlays().dispatch_toast_error(&gettext("Failed to open the file in the file explorer"));
-                    tracing::debug!("opening file {} in the file explorer failed: {e:?}", current_file.uri());
+                    tracing::debug!("opening file {} in the file explorer failed, could not get the path", current_file.uri());
                 }
             }
         }),

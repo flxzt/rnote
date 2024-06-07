0272d1002feff104272569854665b2b172b1e5e9
Doublonmousse <115779707+Doublonmousse@users.noreply.github.com>
2024-06-07 18:51:53 +0200

    reorganize code and better error handling

--- crates/rnote-ui/src/workspacebrowser/workspaceactions/mod.rs
+++ crates/rnote-ui/src/workspacebrowser/workspaceactions/mod.rs
@@ -1,6 +1,7 @@
 // Modules
 mod createfolder;
+mod openfolder;
 
 // Re-exports
 pub(crate) use createfolder::create_folder;
-pub(crate) use createfolder::open_folder;
+pub(crate) use openfolder::open_folder;

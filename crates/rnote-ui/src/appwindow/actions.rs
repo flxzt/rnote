5d5d149a08771fb1bbab258100fcc2ba56dd640c
Doublonmousse <115779707+Doublonmousse@users.noreply.github.com>
2024-07-13 16:38:36 +0200

    close selection tool when deleting in temporary mode

--- crates/rnote-ui/src/appwindow/actions.rs
+++ crates/rnote-ui/src/appwindow/actions.rs
@@ -7,7 +7,7 @@ use gtk4::{
     UriLauncher, Window,
 };
 use p2d::bounding_volume::BoundingVolume;
-use rnote_compose::penevent::ShortcutKey;
+use rnote_compose::penevent::{KeyboardKey, ShortcutKey};
 use rnote_compose::SplitOrder;
 use rnote_engine::engine::StrokeContent;
 use rnote_engine::pens::PenStyle;
@@ -406,7 +406,15 @@ impl RnAppWindow {
         // Trash Selection
         action_selection_trash.connect_activate(clone!(@weak self as appwindow => move |_, _| {
             let canvas = appwindow.active_tab_wrapper().canvas();
-            let widget_flags = canvas.engine_mut().trash_selection();
+            // check if we have a selector as a temporary tool and need to change the pen
+            let cancel_selection = canvas.engine_ref().cancel_selection_temporary_pen();
+            let widget_flags = if cancel_selection {
+                // trigger an event for a KeyboardPress::Delete
+                let (_, widget_flags) = canvas.engine_mut().handle_pen_event(rnote_compose::PenEvent::KeyPressed { keyboard_key: KeyboardKey::Delete, modifier_keys: vec![] }, None, Instant::now());
+                widget_flags
+            } else {
+                canvas.engine_mut().trash_selection()
+            };
             appwindow.handle_widget_flags(widget_flags, &canvas);
         }));
 

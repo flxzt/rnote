86328384b1ca3f1b64075a4dc4b70fa78c4e479b
RayJW <29835902+RayJW@users.noreply.github.com>
2024-08-27 19:00:30 +0200

    Modify trash.rs to spawn confirmation dialog

--- crates/rnote-ui/src/workspacebrowser/filerow/actions/trash.rs
+++ crates/rnote-ui/src/workspacebrowser/filerow/actions/trash.rs
@@ -1,28 +1,15 @@
 // Imports
-use crate::workspacebrowser::RnFileRow;
 use crate::RnAppWindow;
-use gettextrs::gettext;
-use gtk4::{gio, glib, glib::clone, prelude::FileExt};
-use tracing::debug;
+use crate::{dialogs, workspacebrowser::RnFileRow};
+use gtk4::{gio, glib, glib::clone};
 
 /// Create a new `trash` action.
 pub(crate) fn trash(filerow: &RnFileRow, appwindow: &RnAppWindow) -> gio::SimpleAction {
     let action = gio::SimpleAction::new("trash-file", None);
     action.connect_activate(
         clone!(@weak filerow, @weak appwindow => move |_action_trash_file, _| {
-            let Some(current_file) = filerow.current_file() else {
-                return;
-            };
-            current_file.trash_async(
-                glib::source::Priority::DEFAULT,
-                None::<&gio::Cancellable>,
-                clone!(@weak filerow, @strong current_file => move |res| {
-                if let Err(e) = res {
-                    appwindow.overlays().dispatch_toast_error(&gettext("Trashing file failed"));
-                    debug!("Trash filerow file `{current_file:?}` failed , Err: {e:?}");
-                    return;
-                }
-                filerow.set_current_file(None);
+            glib::spawn_future_local(clone!(@weak appwindow => async move {
+                dialogs::dialog_trash_file(&appwindow, &filerow).await;
             }));
         }),
     );

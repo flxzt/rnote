896ab789bc28bbf43d235292b73ae9ae15260f51
RayJW <29835902+RayJW@users.noreply.github.com>
2024-08-27 21:53:56 +0200

    Formatting

--- crates/rnote-ui/src/workspacebrowser/filerow/actions/trash.rs
+++ crates/rnote-ui/src/workspacebrowser/filerow/actions/trash.rs
@@ -6,12 +6,10 @@ use gtk4::{gio, glib, glib::clone};
 /// Create a new `trash` action.
 pub(crate) fn trash(filerow: &RnFileRow, appwindow: &RnAppWindow) -> gio::SimpleAction {
     let action = gio::SimpleAction::new("trash-file", None);
-    action.connect_activate(
-        clone!(@weak filerow, @weak appwindow => move |_action_trash_file, _| {
-            glib::spawn_future_local(clone!(@weak appwindow => async move {
-                dialogs::dialog_trash_file(&appwindow, &filerow).await;
-            }));
-        }),
-    );
+    action.connect_activate(clone!(@weak appwindow, @weak filerow => move |_, _| {
+        glib::spawn_future_local(clone!(@weak appwindow, @weak filerow => async move {
+            dialogs::dialog_trash_file(&appwindow, &filerow).await;
+        }));
+    }));
     action
 }

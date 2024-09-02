b8062ecaf56d3619ffffe381c39dc70012eff3b0
anesthetice <118751106+anesthetice@users.noreply.github.com>
2024-09-02 16:36:14 +0200

    one-to-one repr of semver::Version in PRELUE

--- crates/rnote-engine/src/fileformats/rnoteformat/mod.rs
+++ crates/rnote-engine/src/fileformats/rnoteformat/mod.rs
@@ -16,6 +16,7 @@ pub use methods::{CompressionMethod, SerializationMethod};
 // Imports
 use super::{FileFormatLoader, FileFormatSaver};
 use crate::engine::{save::SavePrefs, EngineSnapshot};
+use anyhow::Context;
 use legacy::LegacyRnoteFile;
 use maj0min12::RnoteFileMaj0Min12;
 use std::io::Write;
@@ -26,22 +27,36 @@ pub type RnoteHeader = maj0min12::RnoteHeaderMaj0Min12;
 impl RnoteFileMaj0Min12 {
     // ideally, this should never change
     pub const MAGIC_NUMBER: [u8; 9] = [0x52, 0x4e, 0x4f, 0x54, 0x45, 0xce, 0xa6, 0xce, 0x9b];
-    // version not directly linked with CARGO_PKG_VERSION, it should only be updated
-    // (to the newest Rnote version) when changes are made
-    pub const VERSION: [u8; 3] = [0, 12, 0];
-    pub const SEMVER: semver::Version = semver::Version::new(0, 12, 0);
+    pub const SEMVER: &'static str = crate::utils::crate_version();
 }
 
 impl FileFormatSaver for RnoteFile {
     fn save_as_bytes(&self, _file_name: &str) -> anyhow::Result<Vec<u8>> {
+        let version = semver::Version::parse(Self::SEMVER)?;
+        let pre_release = version.pre.as_str();
+        let build_metadata = version.build.as_str();
+
         let json_header = serde_json::to_vec(&ijson::to_value(&self.head)?)?;
         let header = [
             &Self::MAGIC_NUMBER[..],
-            &Self::VERSION[..],
-            &u32::try_from(json_header.len())?.to_le_bytes(),
+            &version.major.to_le_bytes(),
+            &version.minor.to_le_bytes(),
+            &version.patch.to_le_bytes(),
+            &u16::try_from(pre_release.len())
+                .context("Prerelease text is too long")?
+                .to_le_bytes(),
+            pre_release.as_bytes(),
+            &u16::try_from(build_metadata.len())
+                .context("BuildMetadata text is too long")?
+                .to_le_bytes(),
+            build_metadata.as_bytes(),
+            &u32::try_from(json_header.len())
+                .context("Serialized header is too large")?
+                .to_le_bytes(),
             &json_header,
         ]
         .concat();
+
         let mut buffer: Vec<u8> = Vec::new();
         buffer.write_all(&header)?;
         buffer.write_all(&self.body)?;
@@ -54,14 +69,17 @@ impl FileFormatLoader for RnoteFile {
     where
         Self: Sized,
     {
+        let mut prelude_idx: usize = 0;
+
         let magic_number = bytes
-            .get(..9)
+            .get(prelude_idx..9)
             .ok_or_else(|| anyhow::anyhow!("Failed to get magic number"))?;
+        prelude_idx += 9;
 
         if magic_number != Self::MAGIC_NUMBER {
             // Gzip magic number
             // The legacy file is generally caught first in Snapshot::load_from_rnote_bytes
-            // as this conversion is less efficient, but necessary to avoid spaghetti code in rnote-cli
+            // howver this less efficient catch is necessary for rnote-cli mutate
             if magic_number[..2] == [0x1f, 0x8b] {
                 return RnoteFile::try_from(LegacyRnoteFile::load_from_bytes(bytes)?);
             } else {
@@ -69,31 +87,100 @@ impl FileFormatLoader for RnoteFile {
             }
         }
 
-        let mut version: [u8; 3] = [0; 3];
-        version.copy_from_slice(
+        let mut major: [u8; 8] = [0; 8];
+        major.copy_from_slice(
             bytes
-                .get(9..12)
+                .get(prelude_idx..prelude_idx + 8)
                 .ok_or_else(|| anyhow::anyhow!("Failed to get version"))?,
         );
-        let version = semver::Version::new(
-            u64::from(version[0]),
-            u64::from(version[1]),
-            u64::from(version[2]),
+        prelude_idx += 8;
+        let major = u64::from_le_bytes(major);
+
+        let mut minor: [u8; 8] = [0; 8];
+        minor.copy_from_slice(
+            bytes
+                .get(prelude_idx..prelude_idx + 8)
+                .ok_or_else(|| anyhow::anyhow!("Failed to get version"))?,
         );
+        prelude_idx += 8;
+        let minor = u64::from_le_bytes(minor);
+
+        let mut patch: [u8; 8] = [0; 8];
+        patch.copy_from_slice(
+            bytes
+                .get(prelude_idx..prelude_idx + 8)
+                .ok_or_else(|| anyhow::anyhow!("Failed to get version"))?,
+        );
+        prelude_idx += 8;
+        let patch = u64::from_le_bytes(patch);
+
+        let mut pre_release_length: [u8; 2] = [0; 2];
+        pre_release_length.copy_from_slice(
+            bytes
+                .get(prelude_idx..prelude_idx + 2)
+                .ok_or_else(|| anyhow::anyhow!("Failed to get Prerelease length"))?,
+        );
+        prelude_idx += 2;
+        let pre_release_length = usize::from(u16::from_le_bytes(pre_release_length));
+
+        let pre_release = if pre_release_length == 0 {
+            semver::Prerelease::EMPTY
+        } else {
+            let text = core::str::from_utf8(
+                bytes
+                    .get(prelude_idx..prelude_idx + pre_release_length)
+                    .ok_or_else(|| anyhow::anyhow!("Failed to get Prerelease"))?,
+            )?;
+            prelude_idx += pre_release_length;
+            semver::Prerelease::new(text)?
+        };
+
+        let mut build_metadata_length: [u8; 2] = [0; 2];
+        build_metadata_length.copy_from_slice(
+            bytes
+                .get(prelude_idx..prelude_idx + 2)
+                .ok_or_else(|| anyhow::anyhow!("Failed to get BuildMetadata length"))?,
+        );
+        prelude_idx += 2;
+        let build_metadata_length = usize::from(u16::from_le_bytes(build_metadata_length));
+
+        let build_metadata = if build_metadata_length == 0 {
+            semver::BuildMetadata::EMPTY
+        } else {
+            let text = core::str::from_utf8(
+                bytes
+                    .get(prelude_idx..prelude_idx + build_metadata_length)
+                    .ok_or_else(|| anyhow::anyhow!("Failed to get BuildMetadata"))?,
+            )?;
+            prelude_idx += build_metadata_length;
+            semver::BuildMetadata::new(text)?
+        };
+
+        let version = semver::Version {
+            major,
+            minor,
+            patch,
+            pre: pre_release,
+            build: build_metadata,
+        };
 
         let mut header_size: [u8; 4] = [0; 4];
         header_size.copy_from_slice(
             bytes
-                .get(12..16)
+                .get(prelude_idx..prelude_idx + 4)
                 .ok_or_else(|| anyhow::anyhow!("Failed to get header size"))?,
         );
-        let header_size = u32::from_le_bytes(header_size);
+        prelude_idx += 4;
+        let header_size = usize::try_from(u32::from_le_bytes(header_size))
+            .context("Rnote file header is too large")?;
+
         let header_slice = bytes
-            .get(16..16 + usize::try_from(header_size)?)
-            .ok_or_else(|| anyhow::anyhow!("File head missing"))?;
+            .get(prelude_idx..prelude_idx + header_size)
+            .ok_or_else(|| anyhow::anyhow!("File header missing"))?;
+        prelude_idx += header_size;
 
         let body_slice = bytes
-            .get(16 + usize::try_from(header_size)?..)
+            .get(prelude_idx..)
             .ok_or_else(|| anyhow::anyhow!("File body missing"))?;
 
         Ok(Self {
@@ -105,13 +192,16 @@ impl FileFormatLoader for RnoteFile {
 
 impl RnoteHeader {
     fn load_from_slice(slice: &[u8], version: &semver::Version) -> anyhow::Result<Self> {
-        if semver::VersionReq::parse(">=0.12.0")
+        if semver::VersionReq::parse(">=0.11.0")
             .unwrap()
             .matches(version)
         {
             Ok(ijson::from_value(&serde_json::from_slice(slice)?)?)
         } else {
-            Err(anyhow::anyhow!("Unrecognized header version"))
+            Err(anyhow::anyhow!(
+                "Unrecognized version specifier '{}' for header",
+                version
+            ))
         }
     }
 }

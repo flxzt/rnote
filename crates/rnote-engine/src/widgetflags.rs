283f7dfe447bcaea35148e5f90f0f61714712457
Felix Zwettler <f.zwettler@posteo.de>
2024-12-01 08:52:43 +0100

    chore: remove unneeded request animation field in widget flags

--- crates/rnote-engine/src/widgetflags.rs
+++ crates/rnote-engine/src/widgetflags.rs
@@ -26,8 +26,6 @@ pub struct WidgetFlags {
     /// Meaning, when enabled instead of key events, text events are then emitted
     /// for regular unicode text. Used when writing text with the typewriter.
     pub enable_text_preprocessing: Option<bool>,
-    /// Whether an animation frame is requested
-    pub request_animation_frame: bool,
 }
 
 impl Default for WidgetFlags {
@@ -44,7 +42,6 @@ impl Default for WidgetFlags {
             hide_undo: None,
             hide_redo: None,
             enable_text_preprocessing: None,
-            request_animation_frame: false,
         }
     }
 }
@@ -77,6 +74,5 @@ impl std::ops::BitOrAssign for WidgetFlags {
         if rhs.enable_text_preprocessing.is_some() {
             self.enable_text_preprocessing = rhs.enable_text_preprocessing;
         }
-        self.request_animation_frame |= rhs.request_animation_frame;
     }
 }

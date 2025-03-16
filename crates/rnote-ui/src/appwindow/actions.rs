1929b3dd2b51c7dbcf4e0a7d6a63e124a33734d5
madonuko <madonuko@outlook.com>
2025-03-16 17:23:50 +0800

    fix: change set-color-* to <Ctrl><Shift>1, ... instead

--- crates/rnote-ui/src/appwindow/actions.rs
+++ crates/rnote-ui/src/appwindow/actions.rs
@@ -1189,7 +1189,7 @@ impl RnAppWindow {
         (1..=9).for_each(|i| {
             app.set_accels_for_action(
                 &format!("win.set-color-{i}"),
-                &[&format!("<Alt>{i}"), &format!("<Alt>KP_{i}")],
+                &[&format!("<Ctrl><Shift>{i}"), &format!("<Alt>KP_{i}")],
             )
         });
 

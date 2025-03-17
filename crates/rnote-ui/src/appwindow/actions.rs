0d132effd9ff2ab64b910697900522b5381a0246
madonuko <madonuko@outlook.com>
2025-03-18 00:23:47 +0800

    chore: cargo fmt

--- crates/rnote-ui/src/appwindow/actions.rs
+++ crates/rnote-ui/src/appwindow/actions.rs
@@ -1189,7 +1189,10 @@ impl RnAppWindow {
         (1..=9).for_each(|i| {
             app.set_accels_for_action(
                 &format!("win.set-color-{i}"),
-                &[&format!("<Ctrl><Shift>{i}"), &format!("<Ctrl><Shift>KP_{i}")],
+                &[
+                    &format!("<Ctrl><Shift>{i}"),
+                    &format!("<Ctrl><Shift>KP_{i}"),
+                ],
             )
         });
 

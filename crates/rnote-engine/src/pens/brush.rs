cf8eec529708bf79c3e0733578e08f6c6894040d
Doublonmousse <e0949142@u.nus.edu>
2025-04-07 19:46:03 +0200

    fix #888 : store in the `PenHolder` informations for temporary shortcuts and pass to pens
    This adds a `temporary_tool` bool to the `PenHolder` which signifies whether the following sequence occured (for now only with the selector)
    
    1. activates the selector
    2. Select something then lift the pen (triggering an up or cancel event)
    3. go back with the selector activated via a temporary shortcut
    4. Select something else
    
    Then the temporary style will be `true` as going back will trigger again the `handle_pressed_shortcut_key` function on the pen down event.
    
    remove println
    
    rebase fixes

--- crates/rnote-engine/src/pens/brush.rs
+++ crates/rnote-engine/src/pens/brush.rs
@@ -75,6 +75,7 @@ impl PenBehaviour for Brush {
         event: PenEvent,
         now: Instant,
         engine_view: &mut EngineViewMut,
+        _temporary_tool: bool,
     ) -> (EventResult<PenProgress>, WidgetFlags) {
         let mut widget_flags = WidgetFlags::default();
 

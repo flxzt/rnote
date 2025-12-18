422366acecaa6dc2cbe60d50d50fc0e2f6344725
Felix Zwettler <f.zwettler@posteo.de>
2025-12-18 14:24:12 +0100

    chore: update deps, apply latest lints

--- crates/rnote-ui/src/strokewidthpicker/previewstyle.rs
+++ crates/rnote-ui/src/strokewidthpicker/previewstyle.rs
@@ -1,18 +1,13 @@
 // Imports
 use gtk4::glib;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, glib::Enum)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default, glib::Enum)]
 #[enum_type(name = "RnStrokeWidthPreviewType")]
 #[repr(i32)]
 pub(crate) enum StrokeWidthPreviewStyle {
+    #[default]
     #[enum_value(name = "Circle")]
     Circle,
     #[enum_value(name = "RoundedRect")]
     RoundedRect,
-}
-
-impl Default for StrokeWidthPreviewStyle {
-    fn default() -> Self {
-        Self::Circle
-    }
 }

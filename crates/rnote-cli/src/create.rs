6967288ec32239ffca36049dabd68a5ccd8a2794
Felix Zwettler <f.zwettler@posteo.de>
2026-03-12 20:15:13 +0100

    chore: some cleanup

--- crates/rnote-cli/src/create.rs
+++ crates/rnote-cli/src/create.rs
@@ -8,10 +8,11 @@ pub(crate) async fn create_new_file(engine: &mut Engine, rnote_file: &Path) -> a
         .file_name()
         .map(|s| s.to_string_lossy().to_string())
     else {
-        return Err(anyhow::anyhow!("Failed to get filename from rnote_file"));
+        return Err(anyhow::anyhow!(
+            "Failed to get filename from the supplied file '{}'",
+            rnote_file.display()
+        ));
     };
-
-    // export the default/empty engine state.
     let rnote_bytes = engine.save_as_rnote_bytes(rnote_file_name).await??;
     cli::create_overwrite_file_w_bytes(rnote_file, &rnote_bytes).await?;
 
@@ -20,11 +21,8 @@ pub(crate) async fn create_new_file(engine: &mut Engine, rnote_file: &Path) -> a
 
 pub(crate) async fn run_create(input_file: &Path) -> anyhow::Result<()> {
     let mut engine = Engine::default();
-
-    // Pass the Option through to the inner function
     if let Err(e) = create_new_file(&mut engine, input_file).await {
         return Err(e);
     }
-
     Ok(())
 }

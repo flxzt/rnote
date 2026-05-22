b8452e62a1faa29bb7382e51c4e6fc44975fc3d4
ParticularlyPotent <rowan.grady04@gmail.com>
2026-05-21 17:01:11 -0700

    Revised fix for set_property

--- crates/rnote-ui/src/unitentry.rs
+++ crates/rnote-ui/src/unitentry.rs
@@ -188,8 +188,6 @@ mod imp {
 
     impl RnUnitEntry {
         const MIN_VAL_IN_PX: f64 = 1.0;
-        const MIN_VAL_IN_MM: f64 = 0.1;
-        const MIN_VAL_IN_CM: f64 = 0.01;
         const MAX_VAL_IN_PX: f64 = 100_000.0;
 
         const STEP_INCREMENT_PX: f64 = 1.0;
@@ -206,13 +204,7 @@ mod imp {
 
         fn configure_spinner(&self, unit: MeasureUnit, dpi: f64) {
             let min_val = MeasureUnit::convert_measurement(
-                {
-                    match self.unit.get() {
-                        MeasureUnit::Px => Self::MIN_VAL_IN_PX,
-                        MeasureUnit::Mm => Self::MIN_VAL_IN_MM,
-                        MeasureUnit::Cm => Self::MIN_VAL_IN_CM,
-                    }
-                },
+                Self::MIN_VAL_IN_PX,
                 MeasureUnit::Px,
                 dpi,
                 unit,
